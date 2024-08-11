cddb8483ef1b68389705804b1f4e7b274e646fbb
Maria de los Angeles Gomez Castillo <98776670+mariacastillo982@users.noreply.github.com>
2024-08-11 15:46:30 +0300

    Update Correlation_Trans_Prot_T0_T1.R

--- Correlation_Trans_Prot_T0_T1.R
+++ Correlation_Trans_Prot_T0_T1.R
@@ -1,127 +1,87 @@
-
-library("readxl")
+# Load necessary libraries
+library(readxl)
 library(dplyr)
 library(gplots)
 library(ggplot2)
-library(geneplotter)
-library(RColorBrewer)
-library("GEOquery")
-library(genefilter)
+library(GEOquery)
 library(clusterProfiler)
 library(clariomdhumanhsrefseq.db)
 library(clariomdhumanhsrefseqcdf)
 library(pd.clariomdhuman.hs.refseq)
-#transcriptomic_data=read.csv('/Users/mariacastillo/Desktop/HEATSTROKE/DATA CEL FILES/ALL/filtered_normalized_exprs_Symbol_timepoint_Groupname.csv')
-transcriptomic_data= data.frame(exprs)
+library(org.Hs.eg.db) # Added library for AnnotationDbi::select
+
+# Function to read and process data
+read_proteomics_data <- function(file_path) {
+  proteomics_data <- read.csv(file_path)
+  data_ <- AnnotationDbi::select(org.Hs.eg.db, keys = proteomics_data$X, columns = "SYMBOL", keytype = "UNIPROT")
+  colnames(data_)[1] <- "X"
+  proteomics_data <- left_join(data_, proteomics_data, by = "X")
+  proteomics_data <- proteomics_data[!duplicated(proteomics_data$SYMBOL), ]
+  rownames(proteomics_data) <- proteomics_data$SYMBOL
+  proteomics_data <- proteomics_data %>% select(-X, -SYMBOL)
+  return(proteomics_data)
+}
+
+# Example file paths
+proteomics_file <- "data/normalized_data_Standard_Deviation.csv"
+
+# Read data
+transcriptomic_data <- data.frame(exprs)
 transcriptomic_data <- transcriptomic_data[, !grepl("CD", names(transcriptomic_data))]
-#rownames(transcriptomic_data) = transcriptomic_data$X
-#transcriptomic_data$X <- NULL
+proteomics_data <- read_proteomics_data(proteomics_file)
 
-proteomics_data=read.csv('/Users/mariacastillo/Desktop/HEATSTROKE/Proteomics_C_data/PBMC_1st_Injection_DIANN_results20072023/normalized_data_Standard_Deviation.csv')
-data_ = AnnotationDbi::select(org.Hs.eg.db, keys =proteomics_data$X, columns = "SYMBOL", keytype = "UNIPROT")
-colnames(data_)[1] <- "X"
-proteomics_data2 <- left_join(data_,proteomics_data)
-#rownames(proteomics_data) = proteomics_data$X
-#proteomics_data$X <- NULL
+# Find common genes and align data
+common_genes <- intersect(proteomics_data$SYMBOL, rownames(transcriptomic_data))
+trans <- transcriptomic_data[common_genes, ]
+prot <- proteomics_data[common_genes, ]
 
-common_rows <- intersect(proteomics_data2$SYMBOL, rownames(transcriptomic_data))
-indexes <- which(proteomics_data2$SYMBOL %in% common_rows, arr.ind = TRUE)
-trans <- transcriptomic_data[common_rows, ]
-trans$HS.02.Stroke.T1 = NULL
-prot <- proteomics_data2[indexes, ]
-duplicates <- prot$SYMBOL[duplicated(prot$SYMBOL)]
+# Remove duplicates
 prot <- prot[!duplicated(prot$SYMBOL), ]
-rownames(prot) = prot$SYMBOL
-prot$X <- NULL
-prot$SYMBOL = NULL
-column_order_prot <- names(prot)
-trans <- trans[column_order_prot]
+rownames(prot) <- prot$SYMBOL
 
-cor.test(prot$HS.01.Stroke.T0,trans$HS.01.Stroke.T0)
-cor.test(prot$HS.01.Stroke.T1,trans$HS.01.Stroke.T1)
-cor.test(prot$HS.01.Stroke.T0,trans$HS.01.Stroke.T1)
-cor.test(prot$HS.01.Stroke.T1,trans$HS.01.Stroke.T0)
+# Reorder columns
+trans <- trans[colnames(prot)]
 
-cor.test(prot$HS.03.Stroke.T0,trans$HS.03.Stroke.T0)
-cor.test(prot$HS.03.Stroke.T1,trans$HS.03.Stroke.T1)
-cor.test(prot$HS.03.Stroke.T0,trans$HS.03.Stroke.T1)
-cor.test(prot$HS.03.Stroke.T1,trans$HS.03.Stroke.T0)
+# Perform correlation tests
+correlation_results <- lapply(1:ncol(prot), function(i) {
+  cor.test(prot[, i], trans[, i])
+})
 
-cor.test(prot$HS.07.Stroke.T0,trans$HS.07.Stroke.T0)
-cor.test(prot$HS.07.Stroke.T1,trans$HS.07.Stroke.T1)
-cor.test(prot$HS.07.Stroke.T0,trans$HS.07.Stroke.T1)
-cor.test(prot$HS.07.Stroke.T1,trans$HS.07.Stroke.T0)
+# Write correlation results to file
+write.csv(correlation_results, "results/correlation_all_genes.csv")
 
-cor.test(prot$HS.19.Stroke.T0,trans$HS.19.Stroke.T0)
-cor.test(prot$HS.19.Stroke.T1,trans$HS.19.Stroke.T1)
-cor.test(prot$HS.19.Stroke.T0,trans$HS.19.Stroke.T1)
-cor.test(prot$HS.19.Stroke.T1,trans$HS.19.Stroke.T0)
-write.csv(correlation, "/Users/mariacastillo/Desktop/correlation_all_genes.csv")
+# Subset and correlate specific time points
+subset_timepoints <- function(trans, prot, timepoint) {
+  trans_subset <- trans[, grepl(timepoint, names(trans))]
+  prot_subset <- prot[, grepl(timepoint, names(prot))]
+  return(cor(t(prot_subset), t(trans_subset)))
+}
 
-trans_t0=trans[, grepl("T0", names(trans))]
-trans_t1=trans[, grepl("T1", names(trans))]
+correlation_t0_t0 <- subset_timepoints(trans, prot, "T0")
+correlation_t1_t1 <- subset_timepoints(trans, prot, "T1")
 
-prot_t0=prot[, grepl("T0", names(prot))]
-prot_t1=prot[, grepl("T1", names(prot))]
+# Write specific correlation results
+write.csv(correlation_t0_t0, "results/correlation_t0_t0.csv")
+write.csv(correlation_t1_t1, "results/correlation_t1_t1.csv")
 
-correlation_t0_t0 = cor(t(prot_t0),t(trans_t0))
-correlation_t1_t1 = cor(t(prot_t1),t(trans_t1))
-trans_t0$HS.14.Stroke.T0 = NULL
-trans_t0$HS.02.Stroke.T0 = NULL
-trans_t0$HS.16.Stroke.T0 = NULL
-prot_t0$HS.02.Stroke.T0 = NULL
-prot_t0$HS.14.Stroke.T0 = NULL
-prot_t0$HS.16.Stroke.T0 = NULL
-correlation_t0_t1 <- cor(t(prot_t0), t(trans_t1))
+# Differential expression analysis
+process_DE_data <- function(file_path) {
+  de_data <- read.csv(file_path)
+  de_data <- de_data[!duplicated(de_data$SYMBOL), ]
+  rownames(de_data) <- de_data$SYMBOL
+  return(de_data %>% select(-SYMBOL))
+}
 
+proteomic_DE_file <- "data/proteomic_DE.csv"
+transcriptomic_DE_file <- "data/transcriptomic_DE.csv"
 
-# Housekeeping genes
-genes_int <- c("HPRT1", "RPLP0","UBC", "RPL13A",
-                        "HSPA4L", "HSPB1", "HSP90AA1", "HSP90AB1", "HSPA1A", 
-                      "HSPA1B", "HSPD1", "HSPA8", "HSP90B1", "HSPA6", 
-                      "HSPA4", "HSPA9", "HSPA13", "HSPA2", "HSPE1", "HSPA14", 
-                      "HSPH1", "DNAJB1","DNAJC1","DNAJC2", "DNAJC7", "DNAJC5", "DNAJA1", "DNAJA4")
-trans_ss <- trans[genes_int, ]
-prot_ss <- prot[genes_int, ]
+proteomic_DE <- process_DE_data(proteomic_DE_file)
+transcriptomic_DE <- process_DE_data(transcriptomic_DE_file)
 
-correlation = cor(prot_ss,trans_ss)
-write.csv(correlation, "/Users/mariacastillo/Desktop/correlation_target_genes.csv")
+comp <- intersect(rownames(proteomic_DE), rownames(transcriptomic_DE))
 
-trans_t0=trans_ss[, grepl("T0", names(trans_ss))]
-trans_t1=trans_ss[, grepl("T1", names(trans_ss))]
-prot_t0=prot_ss[, grepl("T0", names(prot_ss))]
-prot_t1=prot_ss[, grepl("T1", names(prot_ss))]
+# Correlation of differential expression data
+correlation_DE <- cor(transcriptomic_DE[comp, ], proteomic_DE[comp, ])
 
-correlation_t0_t0 = cor(t(prot_t0),t(trans_t0))
-correlation_t1_t1 = cor(t(prot_t1),t(trans_t1))
-trans_t0$HS.14.Stroke.T0 = NULL
-trans_t0$HS.02.Stroke.T0 = NULL
-trans_t0$HS.16.Stroke.T0 = NULL
-prot_t0$HS.02.Stroke.T0 = NULL
-prot_t0$HS.14.Stroke.T0 = NULL
-prot_t0$HS.16.Stroke.T0 = NULL
-correlation_t1_t0 <- cor(t(trans_t1), t(prot_t0))
-correlation_t0_t1 <- cor(t(trans_t0), t(prot_t1))
-
-write.csv(correlation_t0_t0, "/Users/mariacastillo/Desktop/correlation_t0_t0.csv")
-write.csv(correlation_t1_t1, "/Users/mariacastillo/Desktop/correlation_t1_t1.csv")
-write.csv(correlation_t0_t1, "/Users/mariacastillo/Desktop/correlation_t0_t1.csv")
-write.csv(correlation_t1_t0, "/Users/mariacastillo/Desktop/correlation_t1_t0.csv")
-
-proteomic_DE=read.csv("/Users/mariacastillo/Desktop/HEATSTROKE/correlation transcriptome proteome/proteomic_DE.csv")
-transcriptomic_DE=read.csv("/Users/mariacastillo/Desktop/HEATSTROKE/correlation transcriptome proteome/transcriptomic_DE.csv")
-comp=intersect(proteomic_DE$SYMBOL, transcriptomic_DE$SYMBOL)
-indexes <- which(proteomic_DE$SYMBOL %in% comp, arr.ind = TRUE)
-proteomic_DE <- proteomic_DE[indexes, ]
-proteomic_DE <- proteomic_DE[!duplicated(proteomic_DE$SYMBOL), ]
-rownames(proteomic_DE)=proteomic_DE$SYMBOL
-proteomic_DE$SYMBOL=NULL
-indexes <- which(transcriptomic_DE$SYMBOL %in% comp, arr.ind = TRUE)
-transcriptomic_DE <- transcriptomic_DE[indexes, ]
-transcriptomic_DE <- transcriptomic_DE[!duplicated(transcriptomic_DE$SYMBOL), ]
-rownames(transcriptomic_DE)=transcriptomic_DE$SYMBOL
-transcriptomic_DE$SYMBOL=NULL
-correlation_DE = cor(transcriptomic_DE,proteomic_DE)
-
-
-
+# Save correlation results
+write.csv(correlation_DE, "results/correlation_DE.csv")
